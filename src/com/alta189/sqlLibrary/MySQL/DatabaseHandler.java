af4a078a2f00043658c642161eebfb3953f2a9cb
Antony Derham <antonyderham@gmail.com>
2011-06-10 00:32:09 +0100

    Opens seperate thread for Warrant process and succesfully connects to database for SQL

--- src/com/alta189/sqlLibrary/MySQL/DatabaseHandler.java
+++ src/com/alta189/sqlLibrary/MySQL/DatabaseHandler.java
@@ -11,31 +11,34 @@ import net.districtmine.warrant.WarrantLogger;
 import net.districtmine.warrant.WarrantSql;
 
 public class DatabaseHandler {
-	private WarrantSql core;  
+	private WarrantSql core;
 	private Connection connection;
 	private String dblocation;
 	private String username;
 	private String password;
 	private String database;
-	  
-	  
-	public DatabaseHandler(WarrantSql core, String dbLocation, String database, String username, String password) {
+
+	public DatabaseHandler(WarrantSql core, String dbLocation, String database,
+			String username, String password) {
 		this.core = core;
 		this.dblocation = dbLocation;
 		this.database = database;
 		this.username = username;
 		this.password = password;
 	}
-	  
-	private void openConnection() throws MalformedURLException, InstantiationException, IllegalAccessException {
+
+	private void openConnection() throws MalformedURLException,
+			InstantiationException, IllegalAccessException {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			connection = DriverManager.getConnection("jdbc:mysql://" + dblocation + "/" + database, username, password);
-	    } catch (ClassNotFoundException e) {
-	    	WarrantLogger.mysqlError("ClassNotFoundException! " + e.getMessage());
-	    } catch (SQLException e) {
-	    	WarrantLogger.mysqlError("SQLException! " + e.getMessage());
-	    }
+			connection = DriverManager.getConnection("jdbc:mysql://"
+					+ dblocation + "/" + database, username, password);
+		} catch (ClassNotFoundException e) {
+			WarrantLogger.mysqlError("ClassNotFoundException! "
+					+ e.getMessage());
+		} catch (SQLException e) {
+			WarrantLogger.mysqlError("SQLException! " + e.getMessage());
+		}
 	}
 
 	public Boolean checkConnection() {
@@ -44,141 +47,161 @@ public class DatabaseHandler {
 				openConnection();
 				return true;
 			} catch (MalformedURLException ex) {
-				WarrantLogger.mysqlError("MalformedURLException! " + ex.getMessage());
+				WarrantLogger.mysqlError("MalformedURLException! "
+						+ ex.getMessage());
 			} catch (InstantiationException ex) {
-				WarrantLogger.mysqlError("InstantiationExceptioon! " + ex.getMessage());
+				WarrantLogger.mysqlError("InstantiationExceptioon! "
+						+ ex.getMessage());
 			} catch (IllegalAccessException ex) {
-				WarrantLogger.mysqlError("IllegalAccessException! " + ex.getMessage());
+				WarrantLogger.mysqlError("IllegalAccessException! "
+						+ ex.getMessage());
 			}
 			return false;
-	    }
+		}
 		return true;
-	  }
+	}
 
 	public void closeConnection() {
 		try {
 			if (connection != null)
 				connection.close();
 		} catch (Exception e) {
-			WarrantLogger.mysqlError("Failed to close database connection! " + e.getMessage());
+			WarrantLogger.mysqlError("Failed to close database connection! "
+					+ e.getMessage());
 		}
 	}
-	
-	public Connection getConnection() throws MalformedURLException, InstantiationException, IllegalAccessException {
+
+	public Connection getConnection() throws MalformedURLException,
+			InstantiationException, IllegalAccessException {
 		if (connection == null) {
 			openConnection();
 		}
 		return connection;
 	}
-	
-	public ResultSet sqlQuery(String query) throws MalformedURLException, InstantiationException, IllegalAccessException {
+
+	public ResultSet sqlQuery(String query) throws MalformedURLException,
+			InstantiationException, IllegalAccessException {
 		try {
 			Connection connection = getConnection();
-		    Statement statement = connection.createStatement();
-		    
-		    ResultSet result = statement.executeQuery(query);
-		    
-		    return result;
+			Statement statement = connection.createStatement();
+
+			ResultSet result = statement.executeQuery(query);
+
+			return result;
 		} catch (SQLException ex) {
-			WarrantLogger.mysqlWarning("Error at SQL Query: " + ex.getMessage());
+			WarrantLogger
+					.mysqlWarning("Error at SQL Query: " + ex.getMessage());
 		}
 		return null;
 	}
-	
-	public void insertQuery(String query) throws MalformedURLException, InstantiationException, IllegalAccessException {
+
+	public void insertQuery(String query) throws MalformedURLException,
+			InstantiationException, IllegalAccessException {
 		try {
 			Connection connection = getConnection();
-		    Statement statement = connection.createStatement();
-		    
-		    statement.executeUpdate(query);
-		    
-		    
+			Statement statement = connection.createStatement();
+
+			statement.executeUpdate(query);
+
 		} catch (SQLException ex) {
-			
-				if (!ex.toString().contains("not return ResultSet")) WarrantLogger.mysqlWarning("Error at SQL INSERT Query: " + ex);
-			
-			
+
+			if (!ex.toString().contains("not return ResultSet"))
+				WarrantLogger.mysqlWarning("Error at SQL INSERT Query: " + ex);
+
 		}
 	}
-	
-	public void updateQuery(String query) throws MalformedURLException, InstantiationException, IllegalAccessException {
+
+	public void updateQuery(String query) throws MalformedURLException,
+			InstantiationException, IllegalAccessException {
 		try {
 			Connection connection = getConnection();
-		    Statement statement = connection.createStatement();
-		    
-		    statement.executeUpdate(query);
-		    
-		    
+			Statement statement = connection.createStatement();
+
+			statement.executeUpdate(query);
+
 		} catch (SQLException ex) {
-			
-				if (!ex.toString().contains("not return ResultSet")) WarrantLogger.mysqlWarning("Error at SQL UPDATE Query: " + ex);
-			
+
+			if (!ex.toString().contains("not return ResultSet"))
+				WarrantLogger.mysqlWarning("Error at SQL UPDATE Query: " + ex);
+
 		}
 	}
-	
-	public void deleteQuery(String query) throws MalformedURLException, InstantiationException, IllegalAccessException {
+
+	public void deleteQuery(String query) throws MalformedURLException,
+			InstantiationException, IllegalAccessException {
 		try {
 			Connection connection = getConnection();
-		    Statement statement = connection.createStatement();
-		    
-		    statement.executeUpdate(query);
-		    
-		    
+			Statement statement = connection.createStatement();
+
+			statement.executeUpdate(query);
+
 		} catch (SQLException ex) {
-			
-				if (!ex.toString().contains("not return ResultSet")) WarrantLogger.mysqlWarning("Error at SQL DELETE Query: " + ex);
-			
+
+			if (!ex.toString().contains("not return ResultSet"))
+				WarrantLogger.mysqlWarning("Error at SQL DELETE Query: " + ex);
+
 		}
 	}
-	
-	public Boolean checkTable(String table) throws MalformedURLException, InstantiationException, IllegalAccessException {
+
+	public Boolean checkTable(String table) throws MalformedURLException,
+			InstantiationException, IllegalAccessException {
 		try {
 			Connection connection = getConnection();
-		    Statement statement = connection.createStatement();
-		    
-		    ResultSet result = statement.executeQuery("SELECT * FROM " + table);
-		    
-		    if (result == null) return false;
-		    if (result != null) return true;
+			Statement statement = connection.createStatement();
+
+			ResultSet result = statement.executeQuery("SELECT * FROM " + table);
+
+			if (result == null)
+				return false;
+			if (result != null)
+				return true;
 		} catch (SQLException ex) {
 			if (ex.getMessage().contains("exist")) {
 				return false;
 			} else {
-				WarrantLogger.mysqlWarning("Error at SQL Query: " + ex.getMessage());
+				WarrantLogger.mysqlWarning("Error at SQL Query: "
+						+ ex.getMessage());
 			}
 		}
-		
-		
-		if (sqlQuery("SELECT * FROM " + table) == null) return true;
+
+		if (sqlQuery("SELECT * FROM " + table) == null)
+			return true;
 		return false;
 	}
-	
-	public Boolean wipeTable(String table) throws MalformedURLException, InstantiationException, IllegalAccessException {
+
+	public Boolean wipeTable(String table) throws MalformedURLException,
+			InstantiationException, IllegalAccessException {
 		try {
 			if (!core.checkTable(table)) {
-				WarrantLogger.mysqlError("Error at Wipe Table: table, " + table + ", does not exist");
+				WarrantLogger.mysqlError("Error at Wipe Table: table, " + table
+						+ ", does not exist");
 				return false;
 			}
 			Connection connection = getConnection();
-		    Statement statement = connection.createStatement();
-		    String query = "DELETE FROM " + table + ";";
-		    statement.executeUpdate(query);
-		    
-		    return true;
+			Statement statement = connection.createStatement();
+			String query = "DELETE FROM " + table + ";";
+			statement.executeUpdate(query);
+
+			return true;
 		} catch (SQLException ex) {
-			if (!ex.toString().contains("not return ResultSet")) WarrantLogger.mysqlWarning("Error at SQL WIPE TABLE Query: " + ex);
+			if (!ex.toString().contains("not return ResultSet"))
+				WarrantLogger.mysqlWarning("Error at SQL WIPE TABLE Query: "
+						+ ex);
 			return false;
 		}
 	}
-	
+
 	public Boolean createTable(String query) {
 		try {
-			if (query == null) { WarrantLogger.mysqlError("SQL Create Table query empty."); return false; }
-		    
+			if (query == null) {
+				WarrantLogger.mysqlError("SQL Create Table query empty.");
+				return false;
+			}
+
 			Statement statement = connection.createStatement();
-		    statement.execute(query);
-		    return true;
-		} catch (SQLException ex){
+			statement.execute(query);
+			return true;
+		} catch (SQLException ex) {
 			WarrantLogger.mysqlError(ex.getMessage());
 			return false;
 		}
--- src/net/districtmine/warrant/Warrant.java
+++ src/net/districtmine/warrant/Warrant.java
@@ -11,10 +11,9 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.event.Event.Priority;
 import org.bukkit.event.Event;
 
-
 public class Warrant extends JavaPlugin {
 	public WarrantSql mysql;
-	
+
 	// Plugin
 	private static String name;
 	private static String version;
@@ -22,55 +21,126 @@ public class Warrant extends JavaPlugin {
 	private static WarrantYml config;
 	private static PluginDescriptionFile pd;
 	private static PluginManager pm;
-
+	
+	// Config
+	private static String forum;
+	private static Boolean multitable;
+	private static String sqlhost;
+	private static String sqlport;
+	private static String sqldb;
+	private static String sqluser;
+	private static String sqlpass;
+	
 	private final WarrantListener playerListener = new WarrantListener();
 
 	@Override
 	public void onEnable() {
 		pd = getDescription();
 		pm = getServer().getPluginManager();
-		
+
 		name = pd.getName();
 		version = pd.getVersion();
 		directory = "plugins" + File.separator + name;
 
 		WarrantLogger.initialize(Logger.getLogger("Minecraft"));
-		
-		File configFile = new File(directory + File.separator + "config.yml");
-	    
-        new File(directory).mkdir();
 
-        if(!configFile.exists()){
-            try {
-                // TODO: Copy over default from JAR
-            	//configFile.createNewFile();
-            	WarrantLogger.error("Couldn't find default config.yml! Disabling...");
-            	this.getPluginLoader().disablePlugin(this);
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
-        } else {
-    	    config = new WarrantYml(configFile);
-    	    config.load();
-        }
-        
+		File configFile = new File(directory + File.separator + "config.yml");
+
+		new File(directory).mkdir();
+
+		if (!configFile.exists()) {
+			try {
+				// TODO: Copy over default from JAR
+				// configFile.createNewFile();
+				WarrantLogger
+						.error("Couldn't find default config.yml! Disabling...");
+				this.getPluginLoader().disablePlugin(this);
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+		} else {
+			config = new WarrantYml(configFile);
+			config.load();
+			forum = config.readString("forum.type");
+			setMultitable(config.readBoolean("forum.multitable"));
+			setSqlhost(config.readString("database.host"));
+			setSqlport(config.readString("database.port"));
+			setSqldb(config.readString("database.db"));
+			setSqluser(config.readString("database.user"));
+			setSqlpass(config.readString("database.pass"));
+		}
+
 		WarrantPermissionsHandler.initialize(this);
-		
-		pm.registerEvent(Event.Type.PLAYER_LOGIN, this.playerListener, Priority.Low, this);
-		
-    	WarrantLogger.info("Enabled");
+
+		pm.registerEvent(Event.Type.PLAYER_LOGIN, this.playerListener,
+				Priority.Low, this);
+
+		WarrantLogger.info("Enabled");
+		WarrantLogger.info("Using forum engine '" + forum + "'");
 	}
 
 	@Override
 	public void onDisable() {
-    	WarrantLogger.info("Disabled");
+		WarrantLogger.info("Disabled");
 	}
-	
+
 	public static String getName() {
 		return name;
 	}
-	
+
 	public static String getVersion() {
 		return version;
+	}
+
+	public static WarrantYml getConfig() {
+		return config;
+	}
+
+	public static void setMultitable(Boolean multitable) {
+		Warrant.multitable = multitable;
+	}
+
+	public static Boolean getMultitable() {
+		return multitable;
+	}
+
+	public static void setSqlhost(String sqlhost) {
+		Warrant.sqlhost = sqlhost;
+	}
+
+	public static String getSqlhost() {
+		return sqlhost;
+	}
+
+	public static void setSqlport(String sqlport) {
+		Warrant.sqlport = sqlport;
+	}
+
+	public static String getSqlport() {
+		return sqlport;
+	}
+
+	public static void setSqldb(String sqldb) {
+		Warrant.sqldb = sqldb;
+	}
+
+	public static String getSqldb() {
+		return sqldb;
+	}
+
+	public static void setSqluser(String sqluser) {
+		Warrant.sqluser = sqluser;
+	}
+
+	public static String getSqluser() {
+		return sqluser;
+	}
+
+	public static void setSqlpass(String sqlpass) {
+		Warrant.sqlpass = sqlpass;
+	}
+
+	public static String getSqlpass() {
+		return sqlpass;
 	}
 }
